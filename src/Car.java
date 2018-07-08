54665da559cdf1c61df68d4bd75e3c4dd8a42e63
PC <p.cz@outlook.com>
2018-07-08 17:21:41 +0200

    Pierwsza poprawka przy autach

---
+++ src/Car.java
@@ -0,0 +1,15 @@
+class Car {
+
+    String brand;
+    String model;
+    int year;
+    double capacity;
+
+    Car(String brand, String model, int year, double capacity) {
+
+        this.brand = brand;
+        this.model = model;
+        this.year = year;
+        this.capacity = capacity;
+    }
+}
